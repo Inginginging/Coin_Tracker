f735cbb2369c32b99688b415217f65f303e74041
ing928 <kik928@nate.com>
2022-03-15 21:49:59 +0900

    styled component (Theme)

--- src/App.js
+++ src/App.js
@@ -1,48 +1,22 @@
-import { keyframes } from "styled-components";
 import styled from "styled-components";
 
-const rotateAni = keyframes` //animation
-  0% {
-    transform: rotate(0deg);
-    border: 1px solid black;
-  }
-  50%{
-    border: 5px solid black;
-  }
-  100%{
-    transform: rotate(360deg);
-    border: 1px solid black;
-  }
-`;
-
 const Container = styled.div`
-  display: flex;
-`;
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-const Box = styled.div`
-  background-color: yellowgreen;
-  width: 200px;
-  height: 200px;
-  animation: ${rotateAni} 1s linear infinite;
+  width: 100vw;
+  height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
-  //내가 만든 component를 직접 targeting할 수 있음
-  ${Emoji} {
-    &:hover {
-      font-size: 100px;
-    }
-  }
+  background-color: ${(props) => props.theme.bgColor}; //theme의 property 사용
+`;
+const Title = styled.h1`
+  font-size: 100px;
+  color: ${(props) => props.theme.textColor}; //theme의 property 사용
 `;
 
 function App() {
   return (
     <Container>
-      <Box>
-        <Emoji>😍</Emoji>
-      </Box>
+      <Title>Hello</Title>
     </Container>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { ThemeProvider } from "styled-components";
+
+//theme 객체
+const darkTheme = {
+  textColor: "whitesmoke",
+  bgColor: "#111",
+};
+const lightTheme = {
+  textColor: "#111",
+  bgColor: "whitesmoke",
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
