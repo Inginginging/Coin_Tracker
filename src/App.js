953cd20c3558436fef0117854a999d9739db5f44
ing928 <kik928@nate.com>
2022-03-15 20:58:30 +0900

    styled component (As)

--- src/App.js
+++ src/App.js
@@ -3,20 +3,18 @@ import styled from "styled-components";
 const Container = styled.div`
   display: flex;
 `;
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
+const Btn = styled.button`
+  border-radius: 5px;
   width: 100px;
-  height: 100px;
-`;
-const Circle = styled(Box)`
-  border-radius: 50%;
+  padding: 15px 0;
+  background-color: blue;
 `;
 
 function App() {
   return (
     <Container>
-      <Box bgColor="tomato" />
-      <Circle bgColor="teal" />
+      <Btn />
+      <Btn as="a" href="/" />
     </Container>
   );
 }
