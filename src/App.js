29d9ae73cf4723d8c2a44fd8a53124cb38bb6956
ing928 <kik928@nate.com>
2022-03-15 21:16:58 +0900

    styled component (Animation & Pseudo Selector)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
+import { keyframes } from "styled-components";
 import styled from "styled-components";
+
+const rotateAni = keyframes` //animation
+  0% {
+    transform: rotate(0deg);
+    border: 1px solid black;
+  }
+  50%{
+    border: 5px solid black;
+  }
+  100%{
+    transform: rotate(360deg);
+    border: 1px solid black;
+  }
+`;
 
 const Container = styled.div`
   display: flex;
 `;
-const Btn = styled.button`
-  border-radius: 5px;
-  width: 100px;
-  padding: 15px 0;
-  background-color: blue;
+const Emoji = styled.span`
+  font-size: 36px;
+`;
+const Box = styled.div`
+  background-color: yellowgreen;
+  width: 200px;
+  height: 200px;
+  animation: ${rotateAni} 1s linear infinite;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  //내가 만든 component를 직접 targeting할 수 있음
+  ${Emoji} {
+    &:hover {
+      font-size: 100px;
+    }
+  }
 `;
 
 function App() {
   return (
     <Container>
-      <Btn />
-      <Btn as="a" href="/" />
+      <Box>
+        <Emoji>😍</Emoji>
+      </Box>
     </Container>
   );
 }
