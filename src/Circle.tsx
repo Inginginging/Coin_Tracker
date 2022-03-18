6d83cea67509ec5f9d0517c48f9df8e84e603e8d
ing928 <kik928@nate.com>
2022-03-18 19:14:12 +0900

    Optional Props (?:)

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -3,10 +3,12 @@ import styled from "styled-components";
 //Circle의 interface
 interface ICircle {
   bgColor: string;
+  borderColor?: string;
 }
 //Container의 interface
 interface IContainer {
   bgColor: string;
+  borderColor: string;
 }
 
 const Container = styled.div<IContainer>`
@@ -15,11 +17,18 @@ const Container = styled.div<IContainer>`
   height: 200px;
   border-radius: 50%;
   background-color: ${(props) => props.bgColor};
+  //style을 지정하므로 styled Componenet의 interface는 optional이 되어선 안됨.
+  border: 5px solid ${(props) => props.borderColor};
 `;
 
 //componenet에 interface 적용
-function Circle({ bgColor }: ICircle) {
-  return <Container bgColor={bgColor} />;
+function Circle({ bgColor, borderColor }: ICircle) {
+  return (
+    <Container
+      bgColor={bgColor}
+      borderColor={borderColor ? borderColor : bgColor}
+    />
+  );
 }
 
 export default Circle;
