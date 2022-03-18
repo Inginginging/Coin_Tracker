4b983e4f4ba1eb01620f7d3de5f8287a076bf3f6
ing928 <kik928@nate.com>
2022-03-18 19:33:11 +0900

    TS에서 state typing & event typing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import styled from "styled-components";
-import Circle from "./Circle";
+import React, { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState(""); //""로 초기화 하여 ts에서 알아서 value의 type을 string으로 인식
+  //ts에서 event를 typing 하는 방법
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setValue(event.currentTarget.value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("typed", value);
+  };
   return (
     <div>
-      <Circle bgColor="teal" />
-      <Circle bgColor="tomato" borderColor="Black" />
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="text"
+          value={value}
+          onChange={onChange}
+        />
+        <button>submit</button>
+      </form>
     </div>
   );
 }
